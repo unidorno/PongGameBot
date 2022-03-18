c04d5bfc7657af94a18ff2aedec89e2a6882fdc8
unidorno <42489432+unidorno@users.noreply.github.com>
2022-03-18 21:18:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -348,7 +348,7 @@ function update() {
 	if (player.score >= 1){
 		//player.highscore = player.score;
 		var xmlhttp = new XMLHttpRequest();
-        var url = "https://unidorno.github.io/PongGameBot/index.html/highscore/" + player.score + "?id=" + playerid;
+        var url = "https://unidorno.github.io/PongGameBot/highscore/" + player.score + "?id=" + playerid;
        	var sendingText = document.getElementById("sendingText");
         sendingText.style.display = "block";
 
